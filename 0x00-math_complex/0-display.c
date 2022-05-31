2f9555db5028913e5d5e44fa9df58a6df5d2e9a4
Juan Bocanegra <jbocane6@gmail.com>
2022-05-30 21:40:03 -0500

    [UPDATE] task 5 v.2

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -12,8 +12,6 @@ void display_complex_number(complex c)
 		return;
 	else if (c.im == 0)
 		printf("%.0f\n", c.re);
-	else if (c.re == 0)
-		printf("%.0fi\n", c.im);
 	else
 	{
 		printf("%.0f", c.re);
