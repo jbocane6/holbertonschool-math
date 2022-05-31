9866f20926903fadb3f42ca0866e10a67c4f5f6c
Juan Bocanegra <jbocane6@gmail.com>
2022-05-30 20:23:10 -0500

    [UPDATE] task 0 v.2

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -7,9 +7,18 @@
 
 void display_complex_number(complex c)
 {
-	printf("%.0f", c.re);
-	if (c.im != 0)
-		printf(" + %.0fi\n", c.im);
+	if (c.im == 0 && c.re == 0)
+		return;
+	else if (c.im == 0)
+		printf("%.0f\n", c.re);
+	else if (c.re == 0)
+		printf("%.0fi\n", c.im);
+	else if (c.im == -1)
+		printf("%0.f - i\n", c.re);
+	else if (c.im < -1)
+		printf("%.0f - %.0fi\n", c.re, (-c.im));
+	else if (c.im == 1)
+		printf("%0.f + i\n", c.re);
 	else
-		printf("\n");
+		printf("%0.f + %0.fi\n", c.re, c.im);
 }
