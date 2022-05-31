e927677cb847f008f5c1df3f8b86564d473b3f84
Juan Bocanegra <jbocane6@gmail.com>
2022-05-30 20:46:10 -0500

    [UPDATE] task 0 v.3

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -3,22 +3,27 @@
 /**
  * display_complex_number - Displays the complex numbers.
  * @c: Complex number a + ib represented by two doubles.
+ * Return: Complex number printed.
  */
 
 void display_complex_number(complex c)
 {
-	if (c.im == 0 && c.re == 0)
+	if (c.re == 0 && c.im == 0)
 		return;
 	else if (c.im == 0)
 		printf("%.0f\n", c.re);
 	else if (c.re == 0)
 		printf("%.0fi\n", c.im);
-	else if (c.im == -1)
-		printf("%0.f - i\n", c.re);
-	else if (c.im < -1)
-		printf("%.0f - %.0fi\n", c.re, (-c.im));
-	else if (c.im == 1)
-		printf("%0.f + i\n", c.re);
 	else
-		printf("%0.f + %0.fi\n", c.re, c.im);
+	{
+		printf("%.0f", c.re);
+		if (c.im < 0)
+		{
+			printf(" - ");
+			c.im *= -1;
+		}
+		else
+			printf(" + ");
+		c.im == 1 ? printf("i\n") : printf("%.0fi\n", c.im);
+	}
 }
